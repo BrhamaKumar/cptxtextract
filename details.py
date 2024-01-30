7372bb630233ec6cb4ef1b01e4a235ddf722407a
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-30 23:50:20 +0530

    Delete details.py

--- details.py
+++
@@ -1,9 +0,0 @@
-import os
-
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
