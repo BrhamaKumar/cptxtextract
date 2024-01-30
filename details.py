b2d37d8d5b52d4661f4e3527298952388a14e8ed
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-30 23:51:03 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = 22779671
+api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+bot_token = "6463214541:AAGbeKXvDZQyxHr5w5M14HuSOmZnxM2YMNQ"
+auth_users = [6301693754]
+sudo_user = 6301693754
+log_channel = -1002109153101
+txt_channel = -1002030573349
