bfbde9f0d26661333b139a5e330c5a79f88e4677
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-30 23:39:11 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,8 +2,8 @@ import os
 
 api_id = 22779671
 api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+bot_token = "6463214541:AAGbeKXvDZQyxHr5w5M14HuSOmZnxM2YMNQ"
+auth_users = [6301693754]
+sudo_user = 6301693754
+log_channel = -1002109153101
+txt_channel = -1002030573349
